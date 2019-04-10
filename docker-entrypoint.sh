a5aaadb5685ee5e4766ad247d3016bada6f1cedf
Guox <33855684+dazuimao1990@users.noreply.github.com>
2019-04-10 23:13:25 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,4 @@
 [[ $PAUSE ]] && sleep $PAUSE
 sed -i -e "s/POD_IP/${POD_IP:-'0.0.0.0'}/g" \
        -e "s/HOSTNAME/${HOSTNAME}/g" /usr/share/elasticsearch/config/elasticsearch.yml
-/bin/sh /usr/local/bin/docker-entrypoint.sh eswrapper
 exec $@
