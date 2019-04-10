32eeda7b37d61c5bea5f7dd33780fca488f3bfe6
Guox <33855684+dazuimao1990@users.noreply.github.com>
2019-04-10 23:36:03 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,5 @@
 [[ $DEBUG ]] && set -x
 [[ $PAUSE ]] && sleep $PAUSE
 sed -i -e "s/POD_IP/${POD_IP:-'0.0.0.0'}/g" \
-       -e "s/HOSTNAME/${HOSTNAME}/g" /usr/share/elasticsearch/config/elasticsearch.yml
+       -e "s/HOSTNAME/${HOSTNAME}.${HOSTNAME%-*}.${TENANT_ID}.svc.cluster.local./g" /usr/share/elasticsearch/config/elasticsearch.yml
 exec $@
