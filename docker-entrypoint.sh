08214f9b48919c86b986a3e55e119cddf536f7f6
Guox <33855684+dazuimao1990@users.noreply.github.com>
2019-04-11 00:19:33 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,18 @@
 [[ $PAUSE ]] && sleep $PAUSE
 sed -i -e "s/POD_IP/${POD_IP:-'0.0.0.0'}/g" \
        -e "s/HOSTNAME/${HOSTNAME}.${HOSTNAME%-*}.${TENANT_ID}.svc.cluster.local./g" /usr/share/elasticsearch/config/elasticsearch.yml
+
+if [[ $ES_NODES -gt 1 ]];then
+    sed -i '$a\discovery.zen.ping.unicast.hosts: HOSTS' /usr/share/elasticsearch/config/elasticsearch.yml
+    declare -a hosts
+    for i in $(seq $ES_NODES)
+      do
+        hosts[$i-1]=\"${HOSTNAME%-*}-`expr $i - 1`.${HOSTNAME%-*}.${TENANT_ID}.svc.cluster.local.:9300\"
+      done
+    echo "elsticsearch cluster nodes are ${hosts[@]}"
+    HOSTS=$(echo [${hosts[@]}] | tr ' ' ',')
+    sed -i "s/discovery.zen.ping.unicast.hosts*/discovery.zen.ping.unicast.hosts: ${HOSTS}/g" /usr/share/elasticsearch/config/elasticsearch.yml
+    unset hosts
+fi
+    
 exec $@
