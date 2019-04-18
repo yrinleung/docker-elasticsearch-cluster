e8763a72249085f29cc7937fb8154d8807075a91
guox <guox@goodrain.com>
2019-04-18 17:27:19 +0800

    [REV] optimize

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,9 @@
 [[ $DEBUG ]] && set -x
 
 sed -i -e "s/POD_IP/${POD_IP:-'0.0.0.0'}/g" \
-       -e "s/HOSTNAME/${HOSTNAME}/g" \
-       -e '$a\discovery.zen.ping.unicast.hosts' /usr/share/elasticsearch/config/elasticsearch.yml
+       -e "s/HOSTNAME/${HOSTNAME}/g" /usr/share/elasticsearch/config/elasticsearch.yml
+
+#Create clusters by splicing domain names
 
 # if [[ ${SERVICE_POD_NUM} -gt 1 ]];then
 #     sed -i '$a\discovery.zen.ping.unicast.hosts' /usr/share/elasticsearch/config/elasticsearch.yml
@@ -19,16 +20,12 @@ sed -i -e "s/POD_IP/${POD_IP:-'0.0.0.0'}/g" \
 # fi
 
 #we will fetch nodes'IP by nslookup
-
-nodeip=$(nslookup ${SERVICE_NAME}.${TENANT_ID}.svc.cluster.local | grep "Address" | grep -v "#53" | awk '{print $2}')
-hosts=[
-for i in ${nodeip[@]}
-  do
-    hosts=$hosts\"$i:9300\",
-    echo $hosts
-  done
-HOSTS=$(echo $hosts | sed 's/.$/]/')
-sed -i "s/discovery.zen.ping.unicast.hosts*/discovery.zen.ping.unicast.hosts: ${HOSTS}/g" /usr/share/elasticsearch/config/elasticsearch.yml
+Num_nodes=$(nslookup ${SERVICE_NAME}.${TENANT_ID}.svc.cluster.local | grep "Address" | grep -v "#53" | awk '{print $2}' | wc -l)
+Node_ip=$(nslookup ${SERVICE_NAME}.${TENANT_ID}.svc.cluster.local | grep "Address" | grep -v "#53" | awk '{print $2}')
+if [[ ${Num_nodes} -gt 1 ]];then
+  HOSTS=[$(echo ${Node_ip} | tr ' ' ',')]
+  sed -i "$a\discovery.zen.ping.unicast.hosts: ${HOSTS}" /usr/share/elasticsearch/config/elasticsearch.yml
+fi
 [[ $PAUSE ]] && sleep $PAUSE
     
 exec $@
